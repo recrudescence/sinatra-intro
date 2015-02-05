f68dd8bf64032631b379d983166b44cca95679e6
recrudescence <calvinmwang@gmail.com>
2015-02-05 02:48:05 -0500

    final pre-heroku creation commit

--- app.rb
+++ app.rb
@@ -3,6 +3,11 @@ require 'sinatra/activerecord'
 require './config/environments'	# database config
 require './models/model'	# model class
 
+# So... this web server accepts three urls:
+# ex.com/				=> form where user can add a record
+# ex.com/submit (POST)	=> form contents submitted, new record created
+# ex.com/models			=> displays list of models
+
 get '/' do
 	erb :index
 end
